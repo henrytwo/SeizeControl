9cb4d43b2252fce0ae01bd90e35bde2dbc9d4333
AdamMehdi <adam_mehdi@hotmail.ca>
2018-09-15 20:06:36 -0400

    hai

--- extension/nizar work.py
+++ extension/nizar work.py
@@ -1,32 +1,7 @@
-# from pytube import YouTube
+from pytube import YouTube
 from pprint import *
-# counter=0
-# yt = YouTube('https://www.youtube.com/watch?v=t_5IWiQutlw')
-# qualityList=["360p","144p","240p","480p","720p"]
-# bol=True
-# while bol:
-#     edited=yt.streams.filter(res=qualityList[counter],only_audio=False,mime_type="video/mp4").all()
-#     if len(edited)==0:
-#         counter+=1
-#     else:
-#         bol=False
-# stream=yt.streams.filter(res=qualityList[counter],only_audio=False,mime_type="video/mp4").first()
-# stream.download()
 
-import cv2
-cap = cv2.VideoCapture('C:/Users/nizar/PycharmProjects/Hack-the-North-Thing/extension/The Chainsmokers & Coldplay - Something Just Like This (Lyric).mp4')
-frameList=[]
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    frameList.append(frame)
-    if ret:
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        break
 
-cap.release()
-cv2.destroyAllWindows()
-pprint(frameList)
+
+
 
--- server/videoProcessing.py
+++ server/videoProcessing.py
@@ -15,38 +15,86 @@ from matplotlib import pyplot
 class video:
     def __init__(self, url):
         self.url = url
-        self.vid = 0
-        self.width = 0
-        self.height = 0
-        self.getVid()
+        self.maxDist = 443.405007
+        self.scores = []
+        self.vid = 1
+        self.width = 1
+        self.height = 1
         self.frameArray = []
         self.frameCount = len(self.frameArray)
         self.getFrames()
-        self.maxDist = 443.405007
-        self.scores = []
+
+
+        #self.getScores()
 
     #downloads vid to be processed
     def getVid(self):
         YouTube(self.url).streams.last().download()
-        self.vid = 0
-        self.width = 0
-        self.height = 0
+        self.vid = 1
+        self.width = 1
+        self.height = 1
 
     #Takes video and makes frame array
     def getFrames(self):
         self.frameArray = []
-        self.frameCount = len(self.frameArray)
-        for i in range(10): #Go through all vid frames
-            img = cv2.imread('ex.jpg', 0) #Put frame in here
-            edges = cv2.Canny(img, 1, 500)
-            self.frameArray.append(edges)
 
 
+        counter = -1
+        yt = YouTube(self.url)
+        qualityList = ["360p", "144p", "240p", "480p", "720p"]
+        bol = True
+        edited = []
+        while bol:
+            if len(edited) == 0:
+                counter += 1
+            else:
+                bol = False
+            edited = yt.streams.filter(res=qualityList[counter], only_audio=False, mime_type="video/mp4").all()
+
+
+        stream = yt.streams.filter(res=qualityList[counter], only_audio=False, mime_type="video/mp4").first()
+        stream.download()
+        print(qualityList[counter])
+
+        cap = cv2.VideoCapture(yt.title + ".mp4")
+        frameList = []
+        start = False
+        prevFrame = None
+        ret, frame = cap.read()
+        self.width, self.height = len(frame[0]), len(frame)
+        frameCount = 0
+        print(yt.streams.filter(res=qualityList[counter], only_audio=False, mime_type="video/mp4").first())
+        FPS = 30
+        while (cap.isOpened()):
+
+            prevFrame = frame
+            ret, frame = cap.read()
+            if start and (frameCount % (FPS//4) == 0):
+                score = self.getContrast(frame, prevFrame)
+                self.scores.append(score)
+                self.sendScore()
+                try:
+                    pass
+                except:
+                    pass
+            start = True
+            if ret:
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+
+            frameCount += 1
+        print(self.scores)
+        #os.remove(yt.title+".mp4")
+
+    #Gets colour change using dist formula
     def colourDist(self, col1, col2):
-        return sqrt((col2[0] - col1[0])**2 + (col2[1] - col1[1])**2 + (col2[2] - col1[2])**2)
+        return sqrt((int(col2[0]) - int(col1[0]))**2 + (int(col2[1]) - int(col1[1]))**2 + (int(col2[2]) - int(col1[2]))**2)
 
-    #takes black and white cv edge frame and finds stripe patterns
+    #Takes black and white cv edge frame and finds stripe patterns
     def stripeVal(self, Eframe):
+        print(Eframe[0])
         lineList = [[[0, 0] for j in range(80)] for i in range(45)]
         for i in range(0, self.height, self.height//45):
             for j in range(0, self.width, self.width//80):
@@ -72,49 +120,31 @@ class video:
                 else:
                     lineList[i][j] = None
         score = 0
+        angleTots = [0, 0, 0, 0, 0, 0]
         for i in lineList:
             for j in i:
-                pass
-
-
-
-    def checkerVal(self, Aframe):
-        pass
-
-    #Takes frame and gets adjacent square differences
-    def adjacencyContrast(self, frame):
-        adjFrame = [[0 for i in range(self.width - (j%2))] for j in range(self.height*2-1)]
-        for i in range(0, self.height, 2):
-            for j in range((2*i)%5, self.width, 2):
-                if i < self.height-1:
-                    adjFrame[i][j] = self.colourDist(frame[i][j], frame[i+1][j])
-                if i > 0:
-                    adjFrame[i-1][j] = self.colourDist(frame[i][j], frame[i-1][j])
-                if j < self.width-1:
-                    adjFrame[i][j] = self.colourDist(frame[i][j], frame[i][j+1])
-                if j > 0:
-                    adjFrame[i][j-1] = self.colourDist(frame[i][j], frame[i][j-1])
-        return self.stripeVal(adjFrame) + self.checkerVal(adjFrame)
+                angleTots[int(atan(j[1])//(pi/6))] += j[0]**5
+        s = sqrt(sum([i**1.2 for i in angleTots]))/136 #maximum of 1, value of stripe count found
+        #print(s)
+        return s
 
     #Gets basic contrast of Cframe (0 to 100)
     def totContrast(self, Cframe):
         s = 0
-        for col in Cframe:
-            s += sum(col/(self.width*self.maxDist))
-        s *= 100/self.height
+        for i in Cframe:
+            for col in i:
+                s += col/(self.width*self.maxDist)
+        s /= self.height
+        s = s**0.4
+        s *= 100
         return s
 
     #Sends fixed score to server
-    def sendScore(self, index):
-        start = max(0, index-9)
-        return sum(self.scores[start, index+1])/(index-start+1)
+    def sendScore(self):
+        start = max(0, len(self.scores)-10)
+        print(sum(self.scores[start:len(self.scores)])/(len(self.scores)-start))
+        #Send to database
 
-    #Goes through all contrast frames and generates scores, "main" of the class
-    def getScores(self):
-        for i in range(self.frameCount - 1):
-            score = self.getContrast(self.frameArray[i], self.frameArray[i+1])
-            self.scores.append(score)
-            self.sendScore(i)
 
     #Creates the contrast frame between two frames and gets epilepsy value
     def getContrast(self, frame1, frame2):
@@ -122,13 +152,9 @@ class video:
         for i in range(self.height):
             for j in range(self.width):
                 contrastFrame[i][j] = self.colourDist(frame1[i][j], frame2[i][j])
-        return (self.getContrastScore(contrastFrame) + self.adjacencyContrast(frame1))/2
-
-    #Code to delete the video file downloaded
-    def die(self):
-        os.remove("ChangedFile.csv")
+        cv2.imwrite('cvimg.png', frame1) #Somehow convert pixelarray for frame1 into a png
+        edges = cv2.Canny(frame1, 50, 300)
+        return (self.totContrast(contrastFrame))# + self.stripeVal(edges))/2
 
 
-
-
-test = video("https://www.youtube.com/watch?v=fhiHqz8Ur24")
+test = video("https://www.youtube.com/watch?v=LHCob76kigA&list=RDLHCob76kigA&start_radio=1&t=59")
